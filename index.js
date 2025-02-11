e4bdbb4102958591d4ee7801aa2f5b67172ef3ea
JuDongGeon <ehdrjs981024@gmail.com>
2025-02-11 16:28:33 +0900

    final update

--- index.js
+++ index.js
@@ -18,34 +18,51 @@ const rendering = () => {
       list.splice(i, 1);
       rendering();
     };
+    const edit = document.createElement("button");
+    edit.innerText = "수정";
+    edit.onclick = () => {
+      console.log("수정 시작");
+      div.innerHTML = null;
+      const confirm = document.createElement("button");
+      const cancel = document.createElement("button");
+      confirm.innerText = "수정";
+      cancel.innerText = "취소";
+      const ni = document.createElement("input");
+      ni.style.flex = 1;
+      ni.style.border = "none";
+      ni.style.borderBottom = "1px solid";
+      ni.style.outline = "none";
+      cancel.onclick = () => {
+        console.log("취소 ㄱㄱ");
+        div.innerHTML = null;
+        div.append(p, edit, button);
+      };
+      confirm.onclick = () => {
+        const nv = ni.value;
+        if (nv.length === 0) {
+          alert("아무것도 입력되지 않았습니다.");
+          return ni.focus();
+        }
+        if (nv === list[i]) {
+          alert("변경사항이 없습니다.");
+          return ni.focus();
+        }
+        list[i] = nv;
+        alert("수정되었습니다.");
+        div.innerHTML = null;
+        div.append(p, edit, button);
+        db.setItem("list", JSON.stringify(list));
+        rendering();
+      };
+      div.append(ni, confirm, cancel);
+      ni.focus();
+      ni.value = list[i];
+    };
     const p = document.createElement("p");
     p.innerText = list[i];
     const div = document.createElement("div");
     let isEditing = false;
-    const cancel = document.createElement("button");
-    const confirm = document.createElement("button");
-    cancel.innerText = "취소";
-    confirm.innerText = "수정";
-    const wrap = document.createElement("div");
-    wrap.style.display = "flex";
-    wrap.style.columnGap = 10;
-    cancel.addEventListener("click", () => {
-      isEditing = false;
-      console.log("취소 ㄱㄱ");
-      div.innerHTML = null;
-      div.append(p, button);
-    });
-    const newInput = document.createElement("input");
-    const newValue = newInput.value;
-    confirm.addEventListener("click", () => {
-      list[i] = newValue;
-      isEditing = false;
-      console.log("수정 끝");
-      div.innerHTML = null;
-      div.append(p, button);
-      alert("수정되었습니다.");
-    });
-    wrap.append(confirm, cancel);
+    div.append(p, edit, button);
     const li = document.createElement("li");
     li.append(div);
     ul.append(li);
